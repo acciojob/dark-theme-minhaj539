7888d049c1c27c1043d2f201bf7e6452dec2a310
minhaj539 <115364661+minhaj539@users.noreply.github.com>
2023-05-21 21:21:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+document.getElementById("swap").addEventListener("click",swapTheme);
 
+function swapTheme(){
+	let div=document.getElementById("app");
+	let btn=document.getElementById("swap");
+	if(div.className=="day"){
+		div.className="night";
+		btn.className="button_night";
+	}
+	else{
+		div.className="day";
+		btn.className="button_day";
+	}
+}
